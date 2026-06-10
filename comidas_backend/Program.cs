35c631efc639c2540c4e51629f0689d160c8dd60
Jared Peter <73466447+xsky13@users.noreply.github.com>
2026-06-10 15:40:43 -0300

    Revert "Feature/logout"

--- comidas_backend/Program.cs
+++ comidas_backend/Program.cs
@@ -1,68 +1,8 @@
-using System.Text;
-using comidas_backend.Data;
-using comidas_backend.Services;
-using comidas_backend.Services.Impl;
-using comidas_backend.Utils;
-using DotNetEnv;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-
-Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173")
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-        });
-});
-
-builder.Configuration.AddEnvironmentVariables();
-builder.Services.Configure<AuthOptions>(builder.Configuration.GetSection("Auth"));
-
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ComidasDbContext>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = Environment.GetEnvironmentVariable("Auth__Issuer") ?? throw new Exception("No hay issuer"),
-        ValidAudience = Environment.GetEnvironmentVariable("Auth__Audience") ?? throw new Exception("No hay audience"),
-        IssuerSigningKey =
-            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("Auth__Key") ??
-                                                            throw new Exception("No hay key")))
-    };
-    options.Events.OnMessageReceived = context => {
-
-        if (context.Request.Cookies.ContainsKey("X-Access-Token"))
-        {
-            context.Token = context.Request.Cookies["X-Access-Token"];
-        }
-
-        return Task.CompletedTask;
-    };
-});
-
-
-builder.Services.AddScoped<IAuthService, AuthServiceImpl>();
-builder.Services.AddScoped<IUserService, UserServiceImpl>();
-
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -70,18 +10,32 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger(); 
-    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.UseCors();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
