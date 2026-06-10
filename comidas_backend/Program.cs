6f9b3d83a76ee2efb07a19ec9ba47864b6586daa
Jared Peter <aljapeter05@gmail.com>
2026-06-10 11:06:58 -0300

    added user model, initialized db

--- comidas_backend/Program.cs
+++ comidas_backend/Program.cs
@@ -1,10 +1,17 @@
+using comidas_backend.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ComidasDbContext>();
 
 var app = builder.Build();
 
@@ -12,6 +19,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger(); 
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
