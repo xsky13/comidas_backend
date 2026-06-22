f9068a7649ed86b962051a283bcb252e5bc3d9f8
Abiel2310 <abielm2310@gmail.com>
2026-06-22 09:43:19 -0300

    Descripcciones nulables y swagger

--- comidas_backend/Models/Domain/Comida.cs
+++ comidas_backend/Models/Domain/Comida.cs
@@ -4,7 +4,7 @@ public class Comida
 {
     public int Id { get; set; }
     public string Titulo { get; set; }
-    public string Descripcion { get; set; }
+    public string? Descripcion { get; set; }
     public string ImgUrl { get; set; }
     public float PromedioEstrellas { get; set; }
     public int CantidadCalificaciones { get; set; }
--- comidas_backend/Models/Dto/Request/CreateComidaRequestDto.cs
+++ comidas_backend/Models/Dto/Request/CreateComidaRequestDto.cs
@@ -3,6 +3,6 @@ namespace comidas_backend.Models.Dto.Request;
 public class CreateComidaRequestDto
 {
     public string Titulo { get; set; }
-    public string Descripcion { get; set; }
+    public string? Descripcion { get; set; }
     public IFormFile File { get; set; }
 }
